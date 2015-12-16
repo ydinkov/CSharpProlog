1b81ded2db89f14be37d6d912da8b642d7be98fd
jsakamoto <junichi.jp@gmail.com>
2015-12-16 21:43:24 +0900

    chore(package): Create NuGet package, and update assembly version.

--- CSProlog/Properties/AssemblyInfo.cs
+++ CSProlog/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct ("CSProlog")]
-[assembly: AssemblyCopyright("Copyright © 2007-2009 John Pool")]
+[assembly: AssemblyCopyright("Copyright © 2007-2015 John Pool")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("5.0.0.0")]
+[assembly: AssemblyFileVersion("5.0.0.0")]
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# CSharpProlog
+# CSharpProlog [![NuGet Package](https://img.shields.io/nuget/v/CSProlog.svg)](https://www.nuget.org/packages/CSProlog/)
 A C# implementation of Prolog
 
 ## License
