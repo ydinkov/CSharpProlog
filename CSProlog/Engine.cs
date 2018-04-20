92f351dbf6567f70a63ea26d95b2bf69ca94e63d
Josh Ahlstrom <sirahlst@gmail.com>
2018-04-19 23:22:38 -0500

    Added PL.NETcore, a pure dotnet core console app interpreter than runs in linux

--- CSProlog/Engine.cs
+++ CSProlog/Engine.cs
@@ -1808,7 +1808,8 @@ namespace Prolog
 #if NETSTANDARD
           if (enablePersistence)
           {
-            throw new NotImplementedException();
+            //TODO: enable persistence
+            //throw new NotImplementedException();
           }
 #else
           if (enablePersistence)
